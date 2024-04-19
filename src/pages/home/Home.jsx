62270a773bd6c7612a0b030f38376b02d12decf7
Mehak <gargmehak6902@gmail.com>
2024-04-19 17:54:18 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,28 @@
+import Booking from "../bookings/Booking";
+import NewsLetter from "../newsLetter/NewsLetter";
+import Organizer from "./Organizer/Organizer";
+// import Banner from "./Banner";
+// import FeatureEvents from "./FeatureEvents";
+import PopularEvents from "./PopularEvents";
+import MagicBanner from "./magicBanner/MagicBanner";
+// import Faqac from "../../pages/portfolio/faq/Faqac"
+
+const Home = () => {
+  return (
+    <div>
+      <MagicBanner></MagicBanner>
+      {/* <Banner/> */}
+
+      {/* <FeatureEvents/> */}
+      <Organizer />
+      <PopularEvents />
+
+      <Booking></Booking>
+      
+      <NewsLetter></NewsLetter>
+      {/* <Faqac></Faqac> */}
+    </div>
+  );
+};
+
+export default Home;
